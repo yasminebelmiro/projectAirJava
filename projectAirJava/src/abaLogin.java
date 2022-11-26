bccfb5c7f20f43aa2edf7ae7b442e3b300ed8276
yasminebelmiro <118815568+yasminebelmiro@users.noreply.github.com>
2022-11-26 14:10:29 -0300

    Add files via upload

---
+++ projectAirJava/src/abaLogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Yasmine.if
+ */
+public class abaLogin {
+    public static void main(String args[]){
+        loginFrame loginF = new loginFrame();
+        loginF.setVisible(true);
+    }
+    
+}
